5dea1e25e21dba1654f2b5845d348533eced00e5
icnahom <nahom7wos@gmail.com>
2022-01-20 11:56:06 +0300

    Update single.php

--- single.php
+++ single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all single posts
  *
@@ -9,32 +10,37 @@
 
 get_header();
 ?>
+<div class="container pt-5">
+    <div class="row">
 
-	<main id="primary" class="site-main">
+        <main id="primary" class="site-main col-md-8">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+            <?php
+			while (have_posts()) :
+				the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part('template-parts/content', get_post_type());
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'abadir' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'abadir' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+				the_post_navigation(
+					array(
+						'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'abadir') . '</span> <span class="nav-title">%title</span>',
+						'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'abadir') . '</span> <span class="nav-title">%title</span>',
+					)
+				);
 
+			endwhile; // End of the loop.
+			?>
+
+        </main><!-- #main -->
+        <div class="col-md-4">
+            <?php 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				if (comments_open() || get_comments_number()) :
+					comments_template();
+				endif;
 
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+			?>
+        </div>
+    </div>
+    <?php
+	get_footer();
