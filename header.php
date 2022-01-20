09ce513375bb18eaf5d394ae60a3007af0a03f33
Henokked <henok0ed@gmail.com>
2022-01-20 01:53:06 -0800

    update header.php
    header

--- header.php
+++ header.php
@@ -12,48 +12,49 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'abadir' ); ?></a>
+    <?php wp_body_open(); ?>
+    <div id="page" class="site">
+        <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'abadir' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$abadir_description = get_bloginfo( 'description', 'display' );
-			if ( $abadir_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $abadir_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+        <nav id="masthead" class="navbar navbar-expand-md navbar-dark bg-info col site-header">
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'abadir' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+            <div class="site-branding">
+                <?php
+					the_custom_logo();
+					?>
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand"
+                    rel="home"><?php bloginfo( 'name' ); ?></a>
+            </div><!-- .site-branding -->
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- <div id="site-navigation" class="main-navigation"> -->
+            <!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'abadir' ); ?></button> -->
+            <div id="navbarNav" class="collapse navbar-collapse">
+                <?php
+                wp_nav_menu(
+                array(
+                'menu' => 'menu-1',
+                'theme_location' => 'menu-1',
+                'container' => 'ul',
+                'menu_class' => 'navbar-nav',
+                'li_class' => 'nav-item',
+                'a_class' => 'nav-link',
+                'active_class' => 'active',
+                ));
+                ?>
+            </div>
+        </nav><!-- #masthead -->
