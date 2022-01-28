a608a7b93e80b3fdb41543bc238c0797d1290c01
Henokked <henok0ed@gmail.com>
2022-01-28 03:39:58 -0800

    header.php

--- header.php
+++ header.php
@@ -26,34 +26,36 @@
     <div id="page" class="site">
         <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'abadir' ); ?></a>
 
-        <nav id="masthead" class="navbar navbar-expand-md navbar-dark bg-info col site-header">
+        <nav id="masthead" class="navbar navbar-expand-md navbar-dark bg-info col site-header ">
 
-            <div class="site-branding">
-                <?php
+            <div class="container">
+                <div class="site-branding">
+                    <?php
 					the_custom_logo();
 					?>
-                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand"
-                    rel="home"><?php bloginfo( 'name' ); ?></a>
-            </div><!-- .site-branding -->
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand"
+                        rel="home"><?php bloginfo( 'name' ); ?></a>
+                </div><!-- .site-branding -->
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <!-- <div id="site-navigation" class="main-navigation"> -->
-            <!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'abadir' ); ?></button> -->
-            <div id="navbarNav" class="collapse navbar-collapse">
+                <!-- <div id="site-navigation" class="main-navigation"> -->
+                <!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'abadir' ); ?></button> -->
                 <?php
                 wp_nav_menu(
                 array(
                 'menu' => 'menu-1',
                 'theme_location' => 'menu-1',
-                'container' => 'ul',
-                'menu_class' => 'navbar-nav',
-                'li_class' => 'nav-item',
-                'a_class' => 'nav-link',
-                'active_class' => 'active',
+                'depth'           => 1, // 1 = no dropdowns, 2 = with dropdowns.
+                'container'       => 'div',
+                'container_class' => 'collapse navbar-collapse',
+                'container_id'    => 'bs-example-navbar-collapse-1',
+                'menu_class'      => 'navbar-nav mr-auto',
+                 'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'          => new WP_Bootstrap_Navwalker(),
                 ));
                 ?>
             </div>
